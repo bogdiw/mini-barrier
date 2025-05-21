1fd1e7fb2b5576cd0b04ced4fc9e1b6a91076d45
Fratiman <bogdanfrman@yahoo.com>
2025-05-21 22:12:15 +0300

    Added Distance Sensor

--- minibarrier.ino
+++ minibarrier.ino
@@ -8,29 +8,64 @@ const int greenLED = 13;
 const int redLED = 12;
 const int buttonPin = 11;
 
+const int echoPin = 10;
+const int trigPin = 9;
+
+const float DIST_PRAG = 15.0;
+
 void setup() {
   lcd.init();
   lcd.backlight();
-  lcd.print("AJUTOR");
 
   pinMode(redLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
   pinMode(buttonPin, INPUT);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  lcd.setCursor(0, 0);
+  lcd.print("Sistem pornit...");
+  delay(1000);
 }
 
 void loop() {
   int buttonState = digitalRead(buttonPin);
 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+
+  float distance = duration * 0.034 / 2;
+
+  // Afișăm pe LCD
+  lcd.setCursor(0, 0);
+  lcd.print("Dist: ");
+  lcd.print(distance);
+  lcd.print(" cm   ");
+  delay(100);
+
+  lcd.setCursor(0, 1);
+
   if (buttonState == HIGH) {
     digitalWrite(greenLED, HIGH);
     digitalWrite(redLED, LOW);
-    lcd.setCursor(0, 1);
-    lcd.print("Buton: APASAT  ");
+    lcd.print("Mod: MANUAL      ");
   } else {
-    digitalWrite(greenLED, LOW);
-    digitalWrite(redLED, HIGH);
-    lcd.setCursor(0, 1);
-    lcd.print("Buton: NEAPASAT");
+    // Automat, pe baza senzorului
+    if (distance < DIST_PRAG) {
+      digitalWrite(greenLED, HIGH);
+      digitalWrite(redLED, LOW);
+      lcd.print("Mod: AUTO - OK   ");
+    } else {
+      digitalWrite(greenLED, LOW);
+      digitalWrite(redLED, HIGH);
+      lcd.print("Mod: AUTO - STOP ");
+    }
   }
 
   delay(100);
